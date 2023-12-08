7cfedbdf44798fa00fd9be1f2db6d33d924e8e99
SATHYA SIVAM <98577861+sathyasivamloganathan@users.noreply.github.com>
2023-12-08 10:38:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ import cors from 'cors'
 import categoryRoute from './routes/categoryRoutes.js'
 import productRoute from './routes/productRoute.js'
 import path from "path"
+import { fileURLToPath } from 'url';
 
 dotenv.config();
 
@@ -15,6 +16,8 @@ dotenv.config();
 const app = express();
 connectDb();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 app.use(cors());
 app.use(express.json())
 app.use(morgan('dev'))
